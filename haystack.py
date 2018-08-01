7a33a1c70666114ff254bd266c096d3fe81ad948
tsedberry <tsedberry@hotmail.com>
2018-08-01 15:38:57 -0400

    Update haystack.py

--- haystack.py
+++ haystack.py
@@ -8,8 +8,41 @@ def generate_line(n):
 
 def generate_haystack(letters,lines,pun,filename):
     lines = list(generate_line(letters) for i in range(lines))
-    lines.append(pun.replace(" ","") #remove spaces
+    lines.append(pun.replace(" ","")) #remove spaces
     random.shuffle(lines)
     with open(filename,"w") as fp:
         for line in lines:
             fp.write(line + '\n')
+    return None
+
+#################possible solutions to consider
+with open("haystack.txt") as fp:
+    lines = fp.readlines()
+
+english_letter_frequency = "etaoinsrhldcumfpgwybvkxjqz"
+    
+lines.sort(key=lambda line:line.count("E"),reverse=True)
+lines.sort(key=lambda line:line.count("Z"))
+
+def common(line):
+    return line.count("E") + line.count("T") + line.count("A")
+
+def rare(line):
+    return line.count("Z") + line.count("Q") + line.count("J")
+
+def freq(line):
+    return line.count("E") + line.count("T") + line.count("A") - line.count("Z") - line.count("Q") - line.count("J")
+
+def pvar(line):
+    counter = Counter()
+    for c in line:
+        counter[c] += 1
+    return statistics.pvariance(counter.values(),len(line)/26)
+
+#works like a charm
+score = dict() # dict(Z=1,Q=2,J=3,...,E=26)
+for index,letter in enumerate(reversed(english_letter_frequency.upper())):
+    score[letter] = index+1
+def magic(line):
+    return sum(score[c] for c in line)
+lines.sort(key=magic,reverse=True)
